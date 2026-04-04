a0db76a88fbd0540538b549aa3c38e1d3867c83b
Sravani Gunnu <sravani@Sravanis-MacBook-Pro.local>
2026-04-04 18:19:19 +0530

    changed the code to load dataset

--- src/segnet_model.py
+++ src/segnet_model.py
@@ -9,67 +9,57 @@ import torch.nn.functional as F
 from torchvision import models
 
 
-class EncoderBlock(nn.Module):
-    """Single encoder block: Conv -> BatchNorm -> ReLU -> MaxPool"""
+class ConvBlock(nn.Module):
+    """Convolutional block: Conv -> BatchNorm -> ReLU (no pooling)"""
     
     def __init__(self, in_channels, out_channels):
-        super(EncoderBlock, self).__init__()
+        super(ConvBlock, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
         self.bn = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
     
     def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        x = self.relu(x)
-        x, pool_indices = self.pool(x)
-        return x, pool_indices
-
-
-class DecoderBlock(nn.Module):
-    """Single decoder block: Unpool (using indices) -> Conv -> BatchNorm -> ReLU"""
-    
-    def __init__(self, in_channels, out_channels):
-        super(DecoderBlock, self).__init__()
-        self.unpool = nn.MaxUnpool2d(kernel_size=2, stride=2)
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-    
-    def forward(self, x, pool_indices, output_size=None):
-        # Unpool using the pooling indices from encoder
-        x = self.unpool(x, pool_indices, output_size=output_size)
         x = self.conv(x)
         x = self.bn(x)
         x = self.relu(x)
         return x
 
 
+class EncoderBlock(nn.Module):
+    """Encoder block: Multiple convolutions followed by a single MaxPool with indices"""
+    
+    def __init__(self, in_channels, out_channels, num_convs=2):
+        super(EncoderBlock, self).__init__()
+        self.convs = nn.ModuleList()
+        
+        # First conv changes channels
+        self.convs.append(ConvBlock(in_channels, out_channels))
+        # Remaining convs keep same channel
+        for _ in range(num_convs - 1):
+            self.convs.append(ConvBlock(out_channels, out_channels))
+        
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
+    
+    def forward(self, x):
+        for conv in self.convs:
+            x = conv(x)
+        x, pool_indices = self.pool(x)
+        return x, pool_indices
+
+
 class SegNetEncoder(nn.Module):
-    """SegNet Encoder: VGG16-style encoder with 13 convolutional layers"""
+    """SegNet Encoder: VGG16-style encoder with 13 convolutional layers and 4 pooling ops"""
     
     def __init__(self, in_channels=3, pretrained=True):
         super(SegNetEncoder, self).__init__()
         
-        # Define encoder blocks (mimics VGG16 structure)
-        self.block1_1 = EncoderBlock(in_channels, 64)
-        self.block1_2 = EncoderBlock(64, 64)
-        
-        self.block2_1 = EncoderBlock(64, 128)
-        self.block2_2 = EncoderBlock(128, 128)
-        
-        self.block3_1 = EncoderBlock(128, 256)
-        self.block3_2 = EncoderBlock(256, 256)
-        self.block3_3 = EncoderBlock(256, 256)
-        
-        self.block4_1 = EncoderBlock(256, 512)
-        self.block4_2 = EncoderBlock(512, 512)
-        self.block4_3 = EncoderBlock(512, 512)
-        
-        self.block5_1 = EncoderBlock(512, 512)
-        self.block5_2 = EncoderBlock(512, 512)
-        self.block5_3 = EncoderBlock(512, 512)
+        # Define encoder blocks with correct pooling (1 pooling per block)
+        # Each block contains multiple conv layers followed by ONE pooling
+        self.block1 = EncoderBlock(in_channels, 64, num_convs=2)   # 2 conv layers
+        self.block2 = EncoderBlock(64, 128, num_convs=2)           # 2 conv layers
+        self.block3 = EncoderBlock(128, 256, num_convs=3)          # 3 conv layers
+        self.block4 = EncoderBlock(256, 512, num_convs=3)          # 3 conv layers
+        self.block5 = EncoderBlock(512, 512, num_convs=3)          # 3 conv layers
         
         # Initialize weights using He initialization
         self._init_weights()
@@ -87,34 +77,31 @@ class SegNetEncoder(nn.Module):
     def forward(self, x):
         # Store feature maps and pooling indices for decoder
         features = {}
-        indices = {}
+        indices = []
         
-        # Block 1
-        x, indices['pool1_1'] = self.block1_1(x)
-        x, indices['pool1_2'] = self.block1_2(x)
+        # Block 1: 360x480 -> 180x240
+        x, pool_idx = self.block1(x)
+        indices.append(pool_idx)
         features['pool1'] = x
         
-        # Block 2
-        x, indices['pool2_1'] = self.block2_1(x)
-        x, indices['pool2_2'] = self.block2_2(x)
+        # Block 2: 180x240 -> 90x120
+        x, pool_idx = self.block2(x)
+        indices.append(pool_idx)
         features['pool2'] = x
         
-        # Block 3
-        x, indices['pool3_1'] = self.block3_1(x)
-        x, indices['pool3_2'] = self.block3_2(x)
-        x, indices['pool3_3'] = self.block3_3(x)
+        # Block 3: 90x120 -> 45x60
+        x, pool_idx = self.block3(x)
+        indices.append(pool_idx)
         features['pool3'] = x
         
-        # Block 4
-        x, indices['pool4_1'] = self.block4_1(x)
-        x, indices['pool4_2'] = self.block4_2(x)
-        x, indices['pool4_3'] = self.block4_3(x)
+        # Block 4: 45x60 -> 22x30
+        x, pool_idx = self.block4(x)
+        indices.append(pool_idx)
         features['pool4'] = x
         
-        # Block 5
-        x, indices['pool5_1'] = self.block5_1(x)
-        x, indices['pool5_2'] = self.block5_2(x)
-        x, indices['pool5_3'] = self.block5_3(x)
+        # Block 5: 22x30 -> 11x15
+        x, pool_idx = self.block5(x)
+        indices.append(pool_idx)
         features['pool5'] = x
         
         return x, features, indices
@@ -126,58 +113,66 @@ class SegNetDecoder(nn.Module):
     def __init__(self, num_classes=11):
         super(SegNetDecoder, self).__init__()
         
-        # Block 5 decoder
-        self.block5_3 = DecoderBlock(512, 512)
-        self.block5_2 = DecoderBlock(512, 512)
-        self.block5_1 = DecoderBlock(512, 256)
+        # Block 5 decoder (mirror of block 5 encoder: 3 convs + unpool)
+        self.block5_1 = ConvBlock(512, 512)
+        self.block5_2 = ConvBlock(512, 512)
+        self.block5_3 = ConvBlock(512, 512)
+        self.unpool5 = nn.MaxUnpool2d(kernel_size=2, stride=2)
         
-        # Block 4 decoder
-        self.block4_3 = DecoderBlock(256, 512)
-        self.block4_2 = DecoderBlock(512, 512)
-        self.block4_1 = DecoderBlock(512, 128)
+        # Block 4 decoder (mirror of block 4 encoder: 3 convs + unpool)
+        self.block4_1 = ConvBlock(512, 512)
+        self.block4_2 = ConvBlock(512, 512)
+        self.block4_3 = ConvBlock(512, 256)
+        self.unpool4 = nn.MaxUnpool2d(kernel_size=2, stride=2)
         
-        # Block 3 decoder
-        self.block3_3 = DecoderBlock(128, 256)
-        self.block3_2 = DecoderBlock(256, 256)
-        self.block3_1 = DecoderBlock(256, 128)
+        # Block 3 decoder (mirror of block 3 encoder: 3 convs + unpool)
+        self.block3_1 = ConvBlock(256, 256)
+        self.block3_2 = ConvBlock(256, 256)
+        self.block3_3 = ConvBlock(256, 128)
+        self.unpool3 = nn.MaxUnpool2d(kernel_size=2, stride=2)
         
-        # Block 2 decoder
-        self.block2_2 = DecoderBlock(128, 128)
-        self.block2_1 = DecoderBlock(128, 64)
+        # Block 2 decoder (mirror of block 2 encoder: 2 convs + unpool)
+        self.block2_1 = ConvBlock(128, 128)
+        self.block2_2 = ConvBlock(128, 64)
+        self.unpool2 = nn.MaxUnpool2d(kernel_size=2, stride=2)
         
-        # Block 1 decoder
-        self.block1_2 = DecoderBlock(64, 64)
-        self.block1_1 = DecoderBlock(64, num_classes)
-        
-        # Final classification layer
-        self.classifier = nn.Conv2d(num_classes, num_classes, kernel_size=1)
+        # Block 1 decoder (mirror of block 1 encoder: 2 convs + unpool)
+        self.block1_1 = ConvBlock(64, 64)
+        self.block1_2 = ConvBlock(64, num_classes)
+        self.unpool1 = nn.MaxUnpool2d(kernel_size=2, stride=2)
     
     def forward(self, x, indices):
-        # Block 5 decoder (reverse order)
-        x = self.block5_3(x, indices['pool5_3'], output_size=None)
-        x = self.block5_2(x, indices['pool5_2'], output_size=None)
-        x = self.block5_1(x, indices['pool5_1'], output_size=None)
+        # indices is a list: [pool1_idx, pool2_idx, pool3_idx, pool4_idx, pool5_idx]
+        # We process in reverse order
         
-        # Block 4 decoder
-        x = self.block4_3(x, indices['pool4_3'], output_size=None)
-        x = self.block4_2(x, indices['pool4_2'], output_size=None)
-        x = self.block4_1(x, indices['pool4_1'], output_size=None)
+        # Block 5 decoder (unpool 5, then 3 convs)
+        x = self.unpool5(x, indices[4])  # indices[4] is pool5
+        x = self.block5_1(x)
+        x = self.block5_2(x)
+        x = self.block5_3(x)
         
-        # Block 3 decoder
-        x = self.block3_3(x, indices['pool3_3'], output_size=None)
-        x = self.block3_2(x, indices['pool3_2'], output_size=None)
-        x = self.block3_1(x, indices['pool3_1'], output_size=None)
+        # Block 4 decoder (unpool 4, then 3 convs)
+        x = self.unpool4(x, indices[3])  # indices[3] is pool4
+        x = self.block4_1(x)
+        x = self.block4_2(x)
+        x = self.block4_3(x)
         
-        # Block 2 decoder
-        x = self.block2_2(x, indices['pool2_2'], output_size=None)
-        x = self.block2_1(x, indices['pool2_1'], output_size=None)
+        # Block 3 decoder (unpool 3, then 3 convs)
+        x = self.unpool3(x, indices[2])  # indices[2] is pool3
+        x = self.block3_1(x)
+        x = self.block3_2(x)
+        x = self.block3_3(x)
         
-        # Block 1 decoder
-        x = self.block1_2(x, indices['pool1_2'], output_size=None)
-        x = self.block1_1(x, indices['pool1_1'], output_size=None)
+        # Block 2 decoder (unpool 2, then 2 convs)
+        x = self.unpool2(x, indices[1])  # indices[1] is pool2
+        x = self.block2_1(x)
+        x = self.block2_2(x)
         
-        # Final classification
-        x = self.classifier(x)
+        # Block 1 decoder (unpool 1, then 2 convs)
+        x = self.unpool1(x, indices[0])  # indices[0] is pool1
+        x = self.block1_1(x)
+        x = self.block1_2(x)
+        
         return x
 
 
